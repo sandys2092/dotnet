90d15984ec52dc6c44c8053f00376df16b2278e6
sandys2092 <37583163+sandys2092@users.noreply.github.com>
2018-03-20 19:03:39 +0530

    Add files via upload

---
+++ Index.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Index.aspx.cs" Inherits="Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 92px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table style="width: 100%;">
+                <tr>
+                    <td class="auto-style1">Title</td>
+                    <td>
+                        <asp:TextBox ID="txtTtile" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td class="auto-style1">Content</td>
+                    <td>
+                        <asp:TextBox ID="txtContent" runat="server"></asp:TextBox></td>
+                    
+                </tr>
+                
+            </table>
+        </div>
+        <asp:Button ID="btnSend" runat="server" Text="Send" OnClick="btnSend_Click" />
+    </form>
+</body>
+</html>
---
+++ Index.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Index : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnSend_Click(object sender, EventArgs e)
+    {
+        var request = WebRequest.Create("https://onesignal.com/api/v1/notifications") as HttpWebRequest;
+
+        request.KeepAlive = true;
+        request.Method = "POST";
+        request.ContentType = "application/json; charset=utf-8";
+
+        request.Headers.Add("authorization", "Basic NzM3ZDA4NjItODIzNi00ZmEwLTliZmQtYzhjY2FhMWQ1NGFj");
+
+        var serializer = new JavaScriptSerializer();
+        var obj = new
+        {
+            app_id = "6de93b31-802f-4dcd-9bb6-d9e0e670e6d6",
+            contents = new { en = txtContent.Text },
+            headings = new { en = txtTtile.Text},
+            included_segments = new string[] { "All" }
+        };
+        var param = serializer.Serialize(obj);
+        byte[] byteArray = Encoding.UTF8.GetBytes(param);
+
+        string responseContent = null;
+
+        try
+        {
+            using (var writer = request.GetRequestStream())
+            {
+                writer.Write(byteArray, 0, byteArray.Length);
+            }
+
+            using (var response = request.GetResponse() as HttpWebResponse)
+            {
+                using (var reader = new StreamReader(response.GetResponseStream()))
+                {
+                    responseContent = reader.ReadToEnd();
+                }
+            }
+        }
+        catch (WebException ex)
+        {
+            System.Diagnostics.Debug.WriteLine(ex.Message);
+            System.Diagnostics.Debug.WriteLine(new StreamReader(ex.Response.GetResponseStream()).ReadToEnd());
+        }
+
+        System.Diagnostics.Debug.WriteLine(responseContent);
+    }
+}
